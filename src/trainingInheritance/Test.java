e67fe06ad485c19a02064d988765c1bea0b381bb
soeltan zaki rizaldy <soeltanzaki.rizaldy@photoninfotech.net>
2015-11-09 15:34:14 +0700

    Add to github

---
+++ src/trainingInheritance/Test.java
@@ -0,0 +1,19 @@
+package trainingInheritance;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		makhlukHidup animal = new makhlukHidup();
+		animal.berkembangbiak();
+		animal.bernapas();
+		animal.makan();
+		
+		sapi a = new sapi();
+		a.berkembangbiak();
+		a.bernapas();
+		a.makan();
+
+	}
+
+}
---
+++ src/trainingInheritance/makhlukHidup.java
@@ -0,0 +1,21 @@
+package trainingInheritance;
+
+public class makhlukHidup {
+	String alatPernapasan;
+	String makanan;
+	String caraBerkembangBiak;
+	
+	public void bernapas(){
+		System.out.println("makhluk hidup bernafas untuk hidup ");
+	}
+	
+	public void makan(){
+		System.out.println("Hidup dengan memakan makanan");
+	}
+	
+	
+	public void berkembangbiak(){
+		System.out.println("Berkembangbiak ");
+	}
+
+}
